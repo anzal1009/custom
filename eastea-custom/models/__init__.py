6cdbc37a266204a85e3a325d70417d4873539080
anzal1009 <anzalhameed009@gmail.com>
2022-03-25 17:31:49 +0530

    hide send mail button

--- eastea-custom/models/__init__.py
+++ eastea-custom/models/__init__.py
@@ -12,3 +12,13 @@ class SaleOrderInherit(models.Model):
 
     def action_print_purchase_invoice(self):
         return self.env.ref('eastea-custom.purchase_order_report_qweb_paperform1').report_action(self)
+
+
+
+
+#
+class SaleOrderInherit1(models.Model):
+    _inherit = 'sale.order'
+
+#     def action_quotation_send(self):
+#         print('yyy')
